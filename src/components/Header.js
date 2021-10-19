07506c8a83b69a53f77db56b4d56db5671d73803
Harsh-2811 <hrpatel8935@gmail.com>
2021-10-19 22:18:06 +0530

    navs

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,10 +25,30 @@ const Header = ()=>{
 
     return(
        
-        <div>
+        <div className="header fixed-top">
            
-
-           <Navbar id="fullNav" bg="" className={navClass} variant="dark" expand="lg" collapseOnSelect fixed="top">
+           <div className="top-header sm-mx-none bg-primary">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-4">
+                        <ul class="top-contact">
+                            <li><a href="#" class="text-white"><i class="fas fa-phone mr-3" style={{'marginRight':'5px'}}></i> (+91) 8849430206</a></li>
+                            <li><a href="#" class="text-white"><i class="fas fa-envelope mr-3" style={{'marginRight':'5px'}}></i> info@ssvinfotech.in</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-8">
+                        
+                            
+                        <ul class="social-contact ml-auto">
+                            <li style={{'margin-right':'0px !important'}}><a href="https://www.facebook.com/profile.php?id=100069771338478" class="text-white"><i class="fab fa-facebook-f" style={{'padding':'0px'}}></i> </a></li>
+                            <li style={{'margin-right':'0px !important'}}><a href="https://www.instagram.com/ssvinfotech/" class="text-white"><i class="fab fa-instagram" style={{'padding':'0px'}}></i></a></li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+           <Navbar id="fullNav" bg="" className={navClass} variant="dark" expand="lg" collapseOnSelect style={{"backgroundColor":"#ffffff !important"}}>
               <Container>
                 
                     <Navbar.Brand href="/" ><img src="/images/Logo2.png" width="70%"  className="logo" /></Navbar.Brand>
@@ -55,26 +75,26 @@ const Header = ()=>{
                                     }
                                 }  
                                 
-                                ><i className="fas fa-home pr-2"></i>&nbsp; Home</Link>
+                                ><i className="fas fa-home pr-2 mr-1"></i>&nbsp; Home</Link>
                            
                             <LinkBook to='/#aboutme' exact 
                             className={`${location.pathname}${location.hash}` === 
                              "/#aboutme" ? "active animate_link nav-link" : "animate_link nav-link "}>
-                            <i className="fas fa-info pr-2"></i>&nbsp; About Us
+                            <i className="fas fa-info pr-2 mr-1"></i>&nbsp; About Us
                             </LinkBook>
                             <LinkContainer exact to='/services'>
-                                <Nav.Link className="animate_link"><i className="fas fa-server pr-2"></i>&nbsp; Services</Nav.Link>
+                                <Nav.Link className="animate_link"><i className="fas fa-server pr-2 mr-1"></i>&nbsp; Services</Nav.Link>
                             </LinkContainer>
                             
                             <LinkContainer exact to='/projects'>
-                                <Nav.Link className="animate_link"><i className="fas fa-briefcase pr-2"></i>&nbsp; Projects</Nav.Link>
+                                <Nav.Link className="animate_link"><i className="fas fa-briefcase pr-2 mr-1"></i>&nbsp; Projects</Nav.Link>
                                 </LinkContainer>
                             
                             <LinkBook to='/#contacts' exact 
                              className={`${location.pathname}${location.hash}` === 
                              "/#contacts" ? "active animate_link nav-link" : "animate_link nav-link "}
                             >
-                            <i className="fas fa-envelope pr-2 "></i>&nbsp; Contact Us
+                            <i className="fas fa-envelope pr-2 mr-1"></i>&nbsp; Contact Us
 
                             </LinkBook>
                     </Nav>         
